722d8d87390db1e74e845637ec4c5b51a41e605b
simon <simonhong85@gmail.com>
2023-06-07 10:37:15 +0200

    first commit

---
+++ src/main/java/com/marcosimon/autosurvey/AutosurveyApplication.java
@@ -0,0 +1,13 @@
+package com.marcosimon.autosurvey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutosurveyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutosurveyApplication.class, args);
+	}
+
+}
