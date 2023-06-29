878c12d63f04ddc6bc11fb5dd4660df28d35d7b5
simon <simonhong85@gmail.com>
2023-06-29 11:05:23 +0200

    add docker compose

--- src/main/resources/data-postgresql.sql
+++ src/main/resources/data-postgresql.sql
@@ -4,6 +4,6 @@
 --INSERT into country_group (country_id, country) values (2, 'Kenya') ON CONFLICT DO NOTHING;
 --INSERT into country_group (country_id, country) values (3, 'Norway') ON CONFLICT DO NOTHING;
 
-INSERT into organization (org_id, org_name) values (1, 'MSF') ON CONFLICT DO NOTHING;
-INSERT into organization (org_id, org_name) values (2, 'UNDP') ON CONFLICT DO NOTHING;
-INSERT into organization (org_id, org_name) values (3, 'MERCER') ON CONFLICT DO NOTHING;
+--INSERT into organization (org_id, org_name) values (1, 'MSF') ON CONFLICT DO NOTHING;
+--INSERT into organization (org_id, org_name) values (2, 'UNDP') ON CONFLICT DO NOTHING;
+--INSERT into organization (org_id, org_name) values (3, 'MERCER') ON CONFLICT DO NOTHING;
